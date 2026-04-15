b8876cb173ec336a53c9a01ac4dfe622b90a6ad6
Cosmo <Icavalari1972@gmail.com>
2026-04-15 00:48:04 -0400

    Add buildStormDataset - merged discharge, gauge height, precipitation per storm

---
+++ noaa_client.py
@@ -0,0 +1,12 @@
+#usgs_client.py
+#NOAA Client - Kings Point and Bridgeport Tidal Data
+#Port Chester Yacht Club storm event analysis
+#Delaware Engineering, D.P.C
+
+import requests
+import pandas
+
+#Defines the fetchTide function to request data  from NOAA API
+def fetchTide(stationID)
+    https://api.tidesandcurrents.noaa.gov/api/prod/datagetter
+
--- usgs_client.py
+++ usgs_client.py
@@ -7,15 +7,15 @@ import requests
 import pandas
 
 #Defines the fetchStream function to request data from USGS API
-def fetchStream(stationId, startDate, endDate):
+def fetchStream(stationId, startDate, endDate, parameterCd, columnName):
     # Base URL for USGS National Water Information System instantaneous values.
     url = "https://waterservices.usgs.gov/nwis/iv/"
 
-    #Query parameters - Byram River at Pemberwick (01212500), discharge (00060), Date range.
-    #parameterCd 00060 = stream discharge in cubic feet per second
+    #Query parameters - Byram River at Pemberwick (01212500), parameter code, Date range.
+    #parameterCd 00060 = stream discharge (cfs), 00065 = gauge height (ft), 00045 = precipitation (in)
     params = {
         "sites": stationId,
-        "parameterCd": "00060",
+        "parameterCd": parameterCd,
         "startDT": startDate,
         "endDT": endDate,
         "format": "json"
@@ -37,21 +37,29 @@ def fetchStream(stationId, startDate, endDate):
         datetime = reading["dateTime"]
         value = float(reading["value"])
         qualifier = reading["qualifiers"]
-        records.append({"datetime": datetime, "discharge": value, "qualifiers":qualifier})
+        records.append({"datetime": datetime, columnName: value, "qualifiers":qualifier})
 
     #Convert records list to pandas DataFrame
     df = pandas.DataFrame(records)
 
     return df
 
+def buildStormDataset(stormName, startDate, endDate):
+    df_discharge = fetchStream("01212500", startDate, endDate,
+    "00060", "discharge_cfs")
+    df_discharge = df_discharge.rename(columns={"qualifiers": "qualifiers_discharge"})
+    df_gauge = fetchStream("01212500", startDate, endDate, "00065",
+                           "height_ft")
+    df_gauge = df_gauge.rename(columns={"qualifiers":"qualifiers_gauge"})
+    df_precip = fetchStream("410138073394201", startDate, endDate, "00045",
+                            "precipitation_in")
+    df_precip = df_precip.rename(columns={"qualifiers": "qualifiers_precip"})
+    df_merged = pandas.merge(df_discharge, df_gauge, on="datetime")
+    df_merged = pandas.merge(df_merged, df_precip, on="datetime")
+    df_merged.to_csv(path_or_buf=stormName + "_streamData.csv", index=False)
+
 if __name__ == "__main__":
-    df = fetchStream("01212500", "2021-08-29", "2021-09-03")
 
-    #Write DataFrame to CSV - index=False omits row numbers from input.
-    df.to_csv("idaStreamData.csv", index=False)
-
-    df = fetchStream("01212500", "2012-10-27", "2012-10-31")
-    df.to_csv(path_or_buf="sandyStreamData.csv", index=False)
-
-    df = fetchStream("01212500", "2023-09-27", "2023-10-01")
-    df.to_csv(path_or_buf="opheliaStreamData.csv", index=False)
+   buildStormDataset("sandy", "2012-10-27", "2012-10-31")
+   buildStormDataset("ida", "2021-08-29", "2021-09-03")
+   buildStormDataset("ophelia", "2023-09-27", "2023-10-01")
